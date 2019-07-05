288d0e3f9a3fc294802e1e28d40df6872853069f
Nathaniel Brian <nbmbestre91@gmail.com>
2019-07-05 23:44:10 +0800

    header css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { HomeModule } from './home/home.module';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule), canActivate: [ AuthGuard ]}
-  // { path: 'home', loadChildren: () => HomeModule, canActivate: [ AuthGuard ] }
+  // { path: 'home', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule), canActivate: [ AuthGuard ]}
+  { path: 'home', loadChildren: () => HomeModule, canActivate: [ AuthGuard ] }
 ];
 
 @NgModule({
