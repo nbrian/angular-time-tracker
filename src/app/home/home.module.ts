612aacfccaf7f393b6af731c0e6c68c8b7780525
Nathaniel Brian <nbmbestre91@gmail.com>
2019-07-06 00:37:58 +0800

    css and cleanup

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from '../users/users.component';
 import { HeaderComponent } from '../home/header/header.component';
-import { SidebarComponent } from '../home/sidebar/sidebar.component';
 import { FooterComponent } from '../home/footer/footer.component';
 import { UserComponent } from '../user/user.component';
 import { MaterialModule } from '../shared/material.module';
@@ -28,7 +27,7 @@ export const routes: Routes = [
         MaterialModule,
         RouterModule.forChild(routes)
     ],
-    declarations: [HomeComponent, HeaderComponent, SidebarComponent, FooterComponent, UsersComponent, UserComponent],
-    exports: [HomeComponent, HeaderComponent, SidebarComponent, FooterComponent, UsersComponent, UserComponent],
+    declarations: [HomeComponent, HeaderComponent, FooterComponent, UsersComponent, UserComponent],
+    exports: [HomeComponent, HeaderComponent, FooterComponent, UsersComponent, UserComponent],
 })
 export class HomeModule { }
